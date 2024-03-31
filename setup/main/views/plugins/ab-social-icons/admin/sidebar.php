9958ee091eb8491d3dbdea1ec2b195a8389f2beb
kumarinbox31 <kumarinbox31@gmail.com>
2024-03-31 13:27:06 +0530

    social icon plugin ui

---
+++ setup/main/views/plugins/ab-social-icons/admin/sidebar.php
@@ -0,0 +1,9 @@
+<?php 
+    $menu['ab-social-icons'] = array(
+        array(
+            'label' => 'Social Icons',
+            'link'  => 'index.php',
+            'icon'  => 'fa fa-list',
+        ),
+    );
+?>
