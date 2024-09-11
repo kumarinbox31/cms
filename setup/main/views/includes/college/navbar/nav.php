9c9f574805b4d02ba18362ee2947b53fd2191e9b
kumarinbox31 <kumarinbox31@gmail.com>
2024-09-11 12:25:42 +0530

    to fix menu changed code for college theme

--- setup/main/views/includes/college/navbar/nav.php
+++ setup/main/views/includes/college/navbar/nav.php
@@ -2,7 +2,7 @@
 <nav class="navbar navbar-expand-lg" id="navbar">
     <div class="container-fluid">
         <a class="navbar-brand" href="/">
-            <img class="black-logo" src="<?php echo theme_path(); ?>/assets/img/black-logo.png" alt="black-logo">
+            <img class="black-logo" src="<?php echo LOGO ?>" alt="black-logo">
         </a>
         <a class="navbar-toggler text-decoration-none" data-bs-toggle="offcanvas" href="#navbarOffcanvas" role="button"
             aria-controls="navbarOffcanvas">
@@ -13,7 +13,26 @@
             </span>
         </a>
         <div class="collapse navbar-collapse">
-            <ul class="navbar-nav ms-auto">
+            <?
+            $arr = [
+                'id' => '',
+                'class' => 'navbar-nav ms-auto',
+                'itemClass' => 'nav-item',
+                'anchorClass' => 'nav-link',
+                'dropdownUlClass' => 'dropdown-menu',
+                'childItemClass' => 'nav-item',
+                'childAnchorClass' => 'nav-link',
+                'childActiveClass' => 'active',
+                'dropdownLiClass' => 'nav-item',
+                'dropdownAnchorClass' => 'nav-link',
+                'extendBefore' => '',
+                'extendAfter' => '',
+            ];
+            $items = $this->MenuModel->items($id)['items'];
+            print $this->MenuModel->get_menu($items, $arr);
+
+            ?>
+            <!-- <ul class="navbar-nav ms-auto">
 
                 <li class="nav-item">
                     <a href="javascript:void(0)" class="dropdown-toggle nav-link active">
@@ -202,7 +221,7 @@
                         </button>
                     </div>
                 </div>
-            </div>
+            </div> -->
         </div>
     </div>
 </nav>
@@ -211,60 +230,60 @@
 
 <?/*
 <div class="header-area header-area--absolute">
-      <div class="preview-header-inner header-sticky">
-          <div class="container-fluid container-fluid--cp-150">
-              <div class="row">
-                  <div class="col-lg-12">
-                      <div class="header position-relative">
-                          <!-- brand logo -->
-                          <div class="header__logo">
-                              <a href="/">
-                                  <img src="<?php echo LOGO; ?>" aria-label="No Image" width="160" height="48" class="img-fluid light-logo" alt="">
-                                  <img src="<?php echo LOGO; ?>" aria-label="Logo" width="160" height="48" class="img-fluid dark-logo" alt="">
-                              </a>
-                          </div>
-                          <!-- navigation menu -->
-                          <div class="header__navigation menu-style-four preview-menu d-none d-xl-block">
-                              <nav class="navigation-menu navigation-menu--onepage navigation-menu-right">
-                                  <?
-                                  $arr = [
-                                          'id' => '',
-                                          'class' => '',
-                                          'itemClass'    =>  '',
-                                          'anchorClass'  =>  '',
-                                          'dropdownUlClass'   => 'submenu',
-                                          'childItemClass'    => '',
-                                          'childAnchorClass'  => '',
-                                          'childActiveClass'  => 'active',
-                                          'dropdownLiClass'   => 'has-children has-children--multilevel-submenu',
-                                          'dropdownAnchorClass'   => '',
-                                          'extendBefore'      =>  '',
-                                          'extendAfter'       =>  '',
-                                      ];
-                                      $items = $this->MenuModel->items($id)['items'];
-                                      print $this->MenuModel->get_menu($items,$arr);
-                                      
-                                  ?>
-                                  
-                              </nav>
-                          </div>
-                          <!-- header actions -->
-                          <div class="header__actions--preview">
-                              <div class="header__actions">
-                                  <!-- mobile menu -->
-                                  <div id="mobile-menu-trigger" onclick="mobileMenuTrigger()" class="mobile-navigation-icon d-block d-xl-none"><i></i></div>
-                                  <!-- hidden icons menu -->
-                                  <div class="hidden-icons-menu d-block d-md-none" id="hidden-icon-trigger">
-                                      <a href="javascript:void(0)">
-                                          <i class="far fa-ellipsis-h-alt"></i>
-                                      </a>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </div>
-  */ ?>
+    <div class="preview-header-inner header-sticky">
+        <div class="container-fluid container-fluid--cp-150">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="header position-relative">
+                        <!-- brand logo -->
+                        <div class="header__logo">
+                            <a href="/">
+                                <img src="<?php echo LOGO; ?>" aria-label="No Image" width="160" height="48" class="img-fluid light-logo" alt="">
+                                <img src="<?php echo LOGO; ?>" aria-label="Logo" width="160" height="48" class="img-fluid dark-logo" alt="">
+                            </a>
+                        </div>
+                        <!-- navigation menu -->
+                        <div class="header__navigation menu-style-four preview-menu d-none d-xl-block">
+                            <nav class="navigation-menu navigation-menu--onepage navigation-menu-right">
+                                <?
+                                $arr = [
+                                        'id' => '',
+                                        'class' => '',
+                                        'itemClass'    =>  '',
+                                        'anchorClass'  =>  '',
+                                        'dropdownUlClass'   => 'submenu',
+                                        'childItemClass'    => '',
+                                        'childAnchorClass'  => '',
+                                        'childActiveClass'  => 'active',
+                                        'dropdownLiClass'   => 'has-children has-children--multilevel-submenu',
+                                        'dropdownAnchorClass'   => '',
+                                        'extendBefore'      =>  '',
+                                        'extendAfter'       =>  '',
+                                    ];
+                                    $items = $this->MenuModel->items($id)['items'];
+                                    print $this->MenuModel->get_menu($items,$arr);
+                                    
+                                ?>
+                                
+                            </nav>
+                        </div>
+                        <!-- header actions -->
+                        <div class="header__actions--preview">
+                            <div class="header__actions">
+                                <!-- mobile menu -->
+                                <div id="mobile-menu-trigger" onclick="mobileMenuTrigger()" class="mobile-navigation-icon d-block d-xl-none"><i></i></div>
+                                <!-- hidden icons menu -->
+                                <div class="hidden-icons-menu d-block d-md-none" id="hidden-icon-trigger">
+                                    <a href="javascript:void(0)">
+                                        <i class="far fa-ellipsis-h-alt"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+*/ ?>
---
+++ setup/main/views/includes/college/navbar/responsive.php
@@ -0,0 +1,194 @@
+<!-- Start Responsive Navbar Area -->
+<div class="responsive-navbar offcanvas offcanvas-end border-0" data-bs-backdrop="static" tabindex="-1"
+    id="navbarOffcanvas">
+    <div class="offcanvas-header">
+        <a href="index.html" class="logo d-inline-block">
+            <img src="<?php echo LOGO; ?>" alt="logo">
+        </a>
+        <button type="button" class="close-btn bg-transparent position-relative lh-1 p-0 border-0"
+            data-bs-dismiss="offcanvas" aria-label="Close">
+            <i class="ri-close-fill"></i>
+        </button>
+    </div>
+    <div class="offcanvas-body">
+        <?
+        $arr = [
+            'id' => '',
+            'class' => 'responsive-menu',
+            'itemClass' => 'responsive-menu-list without-icon',
+            'anchorClass' => 'nav-link',
+            'dropdownUlClass' => 'responsive-menu-items',
+            'childItemClass' => 'nav-item',
+            'childAnchorClass' => 'nav-link',
+            'childActiveClass' => 'active',
+            'dropdownLiClass' => 'responsive-menu-list',
+            'dropdownAnchorClass' => '',
+            'extendBefore' => '',
+            'extendAfter' => '',
+        ];
+        $items = $this->MenuModel->items($id)['items'];
+        print $this->MenuModel->get_menu($items, $arr);
+
+        ?>
+        <!-- <ul class="responsive-menu">
+
+            <li class="responsive-menu-list active"><a href="javascript:void(0);">Home</a>
+                <ul class="responsive-menu-items">
+                    <li class="nav-item">
+                        <a href="index.html" class="nav-link">
+                            Home Demo - One
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index-2.html" class="nav-link">
+                            Home Demo - Two
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index-3.html" class="nav-link">
+                            Home Demo - Three
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="responsive-menu-list"><a href="javascript:void(0);">Pages</a>
+                <ul class="responsive-menu-items">
+                    <li class="nav-item">
+                        <a href="university-overview.html" class="nav-link">
+                            University Overview
+                        </a>
+                    </li>
+                    <li class="responsive-menu-list"><a href="javascript:void(0);">Blog</a>
+                        <ul class="responsive-menu-items">
+                            <li class="nav-item">
+                                <a href="blog-style-one.html" class="nav-link">
+                                    Blog & News 01
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="blog-style-two.html" class="nav-link">
+                                    Blog & News 02
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="blog-details.html" class="nav-link">
+                                    Blog Details
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="register.html" class="nav-link">
+                            Register Now
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="login.html" class="nav-link">
+                            Log In Now
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="application.html" class="nav-link">
+                            Application Form
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="faculty.html" class="nav-link">
+                            Faculty
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="privacy-policy.html" class="nav-link">
+                            Privacy Policy
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="terms-conditions.html" class="nav-link">
+                            Terms & Conditions
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="responsive-menu-list"><a href="javascript:void(0);">Admissions</a>
+                <ul class="responsive-menu-items">
+                    <li class="nav-item">
+                        <a href="apply.html" class="nav-link">
+                            How To Apply
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="tuition-fees.html" class="nav-link">
+                            Tuition & Fees
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="responsive-menu-list"><a href="javascript:void(0);">Academics</a>
+                <ul class="responsive-menu-items">
+                    <li class="nav-item ">
+                        <a href="programs.html" class="nav-link">
+                            All Programs
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a href="program-details.html" class="nav-link">
+                            Program Details
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a href="events.html" class="nav-link">
+                            All Events
+                        </a>
+                    </li>
+                    <li class="nav-item ">
+                        <a href="event-details.html" class="nav-link">
+                            Event Details
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="responsive-menu-list"><a href="javascript:void(0);">Health Care</a>
+                <ul class="responsive-menu-items">
+                    <li class="nav-item ">
+                        <a href="fitness-athletics.html" class="nav-link">
+                            Fitness & Athletics
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="responsive-menu-list"><a href="javascript:void(0);">Student Life</a>
+                <ul class="responsive-menu-items">
+                    <li class="nav-item ">
+                        <a href="university-life.html" class="nav-link">
+                            University Life
+                        </a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="responsive-menu-list without-icon">
+                <a href="contact.html" class="nav-link">
+                    Contact <span>Us</span>
+                </a>
+            </li>
+        </ul>
+
+        <div class="others-option d-md-flex align-items-center">
+            <div class="option-item">
+                <form class="search-form">
+                    <div class="form-group">
+                        <input class="form-control" type="text" placeholder="Search Programs">
+                        <button type="submit"><i class="ri-search-line"></i></button>
+                    </div>
+                </form>
+            </div>
+        </div> -->
+
+    </div>
+</div>
+<!-- End Responsive Navbar Area -->
