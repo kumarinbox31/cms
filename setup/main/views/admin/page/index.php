735e6452eb671606aa803151bc66e01e34c7629b
Abhijeet Kumar <kumarinbox31@gmail.com>
2026-06-20 13:47:07 +0530

    issue fixed

--- setup/main/views/admin/page/index.php
+++ setup/main/views/admin/page/index.php
@@ -61,7 +61,7 @@
                                 <td>
                                     <a href="'.base_url('admin/page/setting/'.$page->id).'" class="btn btn-sm btn-primary" title="Settings"><i class="fa fa-cog"></i></a>
                                     <a href="'.base_url('admin/editor?type=page&pageid=').$page->id.'" class="btn btn-info btn-sm" title="Normal Editor"><i class="fa fa-edit"></i></a>
-                                    <a href="'.base_url('Ai/editor/page/').$page->id.'" class="btn btn-success btn-sm" title="AI Builder Pro"><i class="fa fa-magic"></i> AI</a>
+                                    <!-- <a href="'.base_url('Ai/editor/page/').$page->id.'" class="btn btn-success btn-sm" title="AI Builder Pro"><i class="fa fa-magic"></i> AI</a> -->
                                     <a onclick="return confirm('."'Are you sure?'".');" href="'.base_url('admin/page?action=trash&id=').$page->id.'" class="btn btn-sm btn-danger" title="Trash"><i class="fa fa-trash"></i></a>
                                 
                                 </td>
